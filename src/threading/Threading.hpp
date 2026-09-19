1beb830d825e52976f67b88995c2fbe049022daf
Simon Stuart <lakraven@gmail.com>
2026-09-19 22:31:52 +0200

    #1 Export Task and Thread public handles

--- src/threading/Threading.hpp
+++ src/threading/Threading.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "Task.hpp"
 #include "TaskCompletion.hpp"
 #include "TaskResults.hpp"
+#include "Thread.hpp"
 #include "ThreadingComposition.hpp"
 #include "ThreadingTypes.hpp"
