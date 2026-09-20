1ad3412afc06587f02e920853a39eda2386693df
Simon Stuart <lakraven@gmail.com>
2026-09-20 11:34:24 +0200

    #1 Restore public static Threading runtime alias

--- src/threading/Threading.hpp
+++ src/threading/Threading.hpp
@@ -7,3 +7,19 @@
 #include "ThreadingComposition.hpp"
 #include "detail/StaticTopologyOwner.hpp"
 #include "ThreadingTypes.hpp"
+
+
+namespace ESPressio::Threading {
+
+    /// Concrete statically owned runtime for one declared Threading topology.
+    template<class TTopology, class TBindings, class TSignalProvider, class TExecutionContextProvider, class TAtomicWord8Provider, class TMutexProvider>
+    using StaticThreadingRuntime = Detail::StaticTopologyOwner<
+        TTopology,
+        TBindings,
+        TSignalProvider,
+        TExecutionContextProvider,
+        TAtomicWord8Provider,
+        TMutexProvider
+    >;
+
+} // ESPressio::Threading
