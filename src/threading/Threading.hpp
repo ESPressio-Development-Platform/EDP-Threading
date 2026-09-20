509f343a6937c7c7a1e811b9cbd3db224f7e8f51
Simon Stuart <lakraven@gmail.com>
2026-09-20 13:52:21 +0200

    #1 Expose topology AtomicWord32 lifecycle provider

--- src/threading/Threading.hpp
+++ src/threading/Threading.hpp
@@ -12,13 +12,13 @@
 namespace ESPressio::Threading {
 
     /// Concrete statically owned runtime for one declared Threading topology.
-    template<class TTopology, class TBindings, class TSignalProvider, class TExecutionContextProvider, class TAtomicWord8Provider, class TMutexProvider>
+    template<class TTopology, class TBindings, class TSignalProvider, class TExecutionContextProvider, class TAtomicWord32Provider, class TMutexProvider>
     using StaticThreadingRuntime = Detail::StaticTopologyOwner<
         TTopology,
         TBindings,
         TSignalProvider,
         TExecutionContextProvider,
-        TAtomicWord8Provider,
+        TAtomicWord32Provider,
         TMutexProvider
     >;
 
