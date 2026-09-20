98e410a1481ce7f7ffc3fc5b54dbe4ca2b0c8089
Simon Stuart <lakraven@gmail.com>
2026-09-20 16:27:41 +0200

    #1 Document Threading compile-time members

--- src/threading/detail/StructuralContextResolver.hpp
+++ src/threading/detail/StructuralContextResolver.hpp
@@ -23,6 +23,7 @@ namespace ESPressio::Threading::Detail {
 
         public:
 
+            /// Compact Type used to identify one managed execution context.
             using ContextIndex = typename SmallestIndex<1U>::Type;
 
 
@@ -49,6 +50,7 @@ namespace ESPressio::Threading::Detail {
 
         public:
 
+            /// Compact Type used to identify one managed execution context.
             using ContextIndex =
                 typename ExecutionContextIndexTraits<TContextCapacity>::Type;
 
